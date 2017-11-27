6ddd965486389a80e3ffa51e5445a48787bc5bca
nattsp <drnphillips@gmail.com>
2017-11-28 07:08:20 +1100

    quingram created

--- readin.R
+++ readin.R
@@ -55,14 +55,17 @@ splitNGram <- function(x_DT){
                 c("ngram","phrase","predict","docfreq"))
 }
 
-
+en_US.twitter.txt
 
 con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.blogs.txt", "rb")
 con2 <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.news.txt", "rb")
+con3 <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.twitter.txt", "rb")
 data_blogs <- read_lines(con)
 data_news <- read_lines(con2)
+data_twitter <- read_lines(con3)
 close(con)
 close(con2)
+close(con3)
 
 con <- file("../../Data/obsceneEnglish.txt", "rb")
 profanity <- read_lines(con)
@@ -71,16 +74,20 @@ save(profanity, file = "..\\..\\Data\\profanity.RData")
 
 head(data_blogs)
 length(data_blogs)
+head(data_twitter)
 
 ## Convert to Corpus
 blogsCorpus <- corpus(data_blogs)
 newsCorpus <- corpus(data_news)
+twitterCorpus <- corpus(data_twitter)
 rm(data_blogs)
 rm(data_news)
+rm(data_twitter)
 
 
 summary(blogsCorpus)
 summary(newsCorpus)
+summary(twitterCorpus)
 texts(newsCorpus)[1:20]
 
 
@@ -92,14 +99,19 @@ splitSet602020 <- function(n){
 
 nBlogs <- ndoc(blogsCorpus)
 nNews <- ndoc(newsCorpus)
+nTweets <- ndoc(twitterCorpus)
 ss <- splitSet602020(nBlogs)
 ssNews <- splitSet602020(nNews)
+ssTweets <- splitSet602020(nTweets)
 
 blogsTrain <- corpus_subset(blogsCorpus, subset = (ss==1))
 newsTrain <- corpus_subset(newsCorpus, subset = (ssNews==1))
+tweetsTrain <- corpus_subset(twitterCorpus, subset = (ssTweets == 1))
 
 summary(newsTrain)
+rm(blogsCorpus)
 rm(newsCorpus)
+rm(twitterCorpus)
 
 blogsHoldOut <- corpus_subset(blogsCorpus, subset = (ss == 2))
 blogsTest <- corpus_subset(blogsCorpus, subset = (ss==3))
@@ -109,6 +121,7 @@ blogsTest <- corpus_subset(blogsCorpus, subset = (ss==3))
 ndoc(blogsTrain)
 ndoc(blogsHoldOut)
 ndoc(blogsTest)
+ndoc(tweetsTrain)
 
 writeLines(as.character(mycorpus), con="mycorpus.txt")
 write_lines(as.character(blogsTest), path = "..\\..\\Data\\blogsTest.txt")
@@ -118,24 +131,31 @@ rm(blogsTest)
 
 blogsSentences <- corpus_reshape(blogsTrain, to = "sentences")
 newsSentences <- corpus_reshape(newsTrain, to = "sentences")
+tweetsSentences <- corpus_reshape(tweetsTrain, to = "sentences")
 blogsSentences[[1]]
 View(blogsSentences[c(1:20),])
 View(newsSentences[c(1:20),])
 texts(blogsSentences)[1:20]
 texts(newsSentences)[1:20]
+texts(tweetsSentences)[1:20]
 summary(blogsSentences)
 summary(newsSentences)
 head(blogsSentences)
 
 docvars(blogsSentences, "source") <- "blogs"
 docvars(newsSentences, "source") <- "news"
+docvars(tweetsSentences, "source") <- "tweets"
 summary(blogsSentences)
 summary(newsSentences)
+summary(tweetsSentences)
 
-TrainSentences <- blogsSentences + newsSentences
+TrainSentences <- blogsSentences + newsSentences + tweetsSentences
 corpusTrain <- blogsTrain + newsTrain
 summary(corpusTrain)
 texts(corpusTrain)[1:20]
+rm(blogsTrain)
+rm(corpusTrain)
+rm(tweetsTrain)
 
 options(width = 200)
 kwic(corpusTrain, "terror")
@@ -277,6 +297,18 @@ topfeatures(trainDfm,100)
 
 ### Ngrams
 
+quingramTrain <- dfm(
+    TrainSentences
+    , tolower = TRUE
+    , remove_numbers = TRUE
+    , remove_punct = TRUE
+    , stem = FALSE
+    , ngrams = 5
+    , remove_twitter = TRUE
+    , remove_url = TRUE
+    , concatenator = " "
+    , verbose = TRUE)
+
 trigramTrain <- dfm(
     TrainSentences
     , tolower = TRUE
@@ -313,6 +345,7 @@ unigramTrain <- dfm(
 trigramTrain
 bigramTrain
 unigramTrain
+topfeatures(quingramTrain, 50)
 topfeatures(trigramTrain, 1000)  # top words
 topfeatures(bigramTrain, 200)  # top words
 topfeatures(unigramTrain, 100)
@@ -323,6 +356,10 @@ summary(bigramTrain)
 trigramTrain[1:5, 1:5]
 bigramTrain[1:5, 1:5]
 unigramTrain[1:5, 1:5]
+
+## Trim 
+## quingramTrain for instance is 4.4 Gb
+quingramTrain <- dfm_trim(quingramTrain, min_docfreq = 4)
 
 ### Clean Ngrams
 
@@ -391,10 +428,17 @@ tempTrigram
 rm(tempTrigram)
 
 # Function to bring together all the cleaning steps
+quingramTrain <- cleanFeatures(quingramTrain)
 bigramTrain <- cleanFeatures(bigramTrain)
 unigramTrain <- cleanFeatures(unigramTrain)
 
 ## Really important conversion
+quingramDT <- data.table(
+    ngram = featnames(quingramTrain), 
+    docfreq = docfreq(quingramTrain),
+    keep.rownames = F, 
+    stringsAsFactors = F
+)
 trigramDT <- data.table(
     ngram = featnames(trigramTrain), 
     docfreq = docfreq(trigramTrain),
@@ -414,17 +458,15 @@ unigramDT <- data.table(
     stringsAsFactors = F
 )
 
+quingramDT
 trigramDT
 bigramDT
 unigramDT
 
-# ngrams take up a lot of memory
-rm(trigramTrain)
-rm(bigramTrain)
-rm(unigramTrain)
 
+quingramDT[ngram %like% "beer"]
 
-
+splitNGram(quingramDT)
 splitNGram(trigramDT)
 splitNGram(bigramDT)
 #splitNGram not working
@@ -435,6 +477,7 @@ setcolorder(trigramDT,
 ## Next time make ngrams without "_"
 trigramDT[, c("ngram", "phrase") := c(gsub("_", " ", ngram), gsub("_", " ", phrase))]
 
+save(quingramDT, file = "../../Data/quingramDT.RData")
 save(trigramDT, file = "../../Data/trigramDT.RData")
 save(bigramDT, file = "../../Data/bigramDT.RData")
 save(unigramDT, file = "../../Data/unigramDT.RData")
@@ -442,6 +485,11 @@ load(file = "../../Data/trigramDT.RData")
 load(file = "../../Data/bigramDT.RData")
 load(file = "../../Data/unigramDT.RData")
 
+# ngrams take up a lot of memory
+rm(quingramTrain)
+rm(trigramTrain)
+rm(bigramTrain)
+rm(unigramTrain)
 
 unigramDT[ngram == "love"]
 
